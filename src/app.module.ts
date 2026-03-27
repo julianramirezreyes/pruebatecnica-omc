49fe034b16c2036e427b33796442673120754b71
julianramirezreyes <julianramirezreyes23@gmail.com>
2026-03-27 15:42:53 -0500

    Inclusion de modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LeadsModule } from './modules/leads/leads.module';
+import { AiModule } from './modules/ai/ai.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), LeadsModule, AiModule],
   controllers: [AppController],
   providers: [AppService],
 })
