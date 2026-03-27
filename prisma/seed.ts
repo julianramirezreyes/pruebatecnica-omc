fc2dafc97fd7e6eb8357fb669bcb2a7b667add9a
julianramirezreyes <julianramirezreyes23@gmail.com>
2026-03-27 15:56:33 -0500

    Seed script agregado

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,8 @@
 import { PrismaClient, Fuente } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+});
 
 const leads = [
   {
